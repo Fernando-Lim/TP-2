751cca4986bbdd736ddd74295440a9e09f0aeb74
Fernando-Lim <lim.liete@gmail.com>
2023-07-16 22:36:13 +0700

    Fernando - First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Cara Install
+
+- Composer Install
+- npm install
+- setting .env (untuk database dan mail reset password )
+- php artisan migrate
+- php artisan key:generate
+- php artisan serve
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,67 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}" id="loginForm">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <div class="mt-2">
+            <label for="captcha" class="col-md-2 col-form-label text-md-right">Captcha</label>
+            <div class="col-md-8">
+                <span class="captcha-image">{!! Captcha::img() !!}</span>
+                
+
+            </div>
+        </div>
+
+        <div class="mt-1">
+            <x-text-input id="captcha" class="form-control @error('captcha') is-invalid @enderror" name="captcha" required />
+            @error('captcha')
+                <span class="invalid-feedback" role="alert">
+                    <strong>Wrong Captcha</strong>
+                </span>
+            @enderror
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ml-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
